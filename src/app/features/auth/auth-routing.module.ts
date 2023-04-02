2a787d015fcd28a6419a5b7ab30d8c442d78de43
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-04-02 15:54:47 +0200

    Scaffolding verification pages

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  { path: "verifyEmail", component: VerifyEmailComponent },
 ];
 
 @NgModule({
