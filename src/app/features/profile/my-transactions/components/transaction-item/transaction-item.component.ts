9b8a40968c0c92c1eb72897d26955601451536c7
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-05-22 23:27:39 +0300

    Scaffolding transactions page

---
+++ src/app/features/profile/my-transactions/components/transaction-item/transaction-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Transaction } from 'src/app/core/models/Transaction';
+
+@Component({
+  selector: 'app-transaction-item',
+  templateUrl: './transaction-item.component.html',
+  styleUrls: ['./transaction-item.component.css']
+})
+export class TransactionItemComponent {
+  @Input() transaction: Transaction = new Transaction('', '', '')
+}
--- src/app/features/profile/my-transactions/my-transactions.component.ts
+++ src/app/features/profile/my-transactions/my-transactions.component.ts
@@ -1,8 +1,32 @@
 import { Component } from "@angular/core";
+import { Transaction } from "src/app/core/models/Transaction";
 
 @Component({
   selector: "app-my-transactions",
   templateUrl: "./my-transactions.component.html",
   styleUrls: ["./my-transactions.component.css"],
 })
-export class MyTransactionsComponent {}
+export class MyTransactionsComponent {
+  currentDate = new Date();
+  formatter = new Intl.DateTimeFormat('en-UK', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  });
+  formattedDate: string = this.formatter.format(this.currentDate).toLocaleUpperCase();
+
+  generateUuid(): string {
+    return Math.ceil(Math.random() * 100000).toString()
+  }
+
+  transactions: Transaction[] = [
+    { transactionId: this.generateUuid(), createdAt: this.formattedDate, status: 'cancelled' },
+    { transactionId: this.generateUuid(), createdAt: this.formattedDate, status: 'completed' },
+    { transactionId: this.generateUuid(), createdAt: this.formattedDate, status: 'cancelled' },
+  ]
+
+}
