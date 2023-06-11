0e0adedaa9411179323b6fe71e04aa6833e796fc
mfawky <mfawky07@gmail.com>
2023-06-12 00:15:54 +0300

    Finidhing Translation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 import { CoreRoutingModule } from "./core-routing.module";
 import { HomeModule } from "./pages/home/home.module";
 import { NotFoundModule } from "./pages/not-found/not-found.module";
 import { BeAwareOfComponent } from './pages/be-aware-of/be-aware-of.component';
 import { AuthGuard } from "./guards/auth.guard";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
@@ -20,8 +22,20 @@ import { AuthGuard } from "./guards/auth.guard";
     CoreRoutingModule,
     HomeModule,
     NotFoundModule,
+    TranslateModule.forRoot({
+      defaultLanguage: "en",
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   exports: [HttpClientModule],
   providers: [AuthGuard]
 })
 export class CoreModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
--- src/app/features/advertisement/advertisement.module.ts
+++ src/app/features/advertisement/advertisement.module.ts
@@ -7,6 +7,13 @@ import { CreateAdvertisementComponent } from "./create-advertisement/create-adve
 import { SearchAdvertisementComponent } from "./search-advertisement/search-advertisement.component";
 import { AdvertisementDetailsComponent } from "./advertisement-details/advertisement-details.component";
 import { UpdateAdvertisementComponent } from './update-advertisement/update-advertisement.component';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { HttpClient } from "@angular/common/http";
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -15,6 +22,13 @@ import { UpdateAdvertisementComponent } from './update-advertisement/update-adve
     AdvertisementDetailsComponent,
     UpdateAdvertisementComponent,
   ],
-  imports: [CommonModule, AdvertisementRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, AdvertisementRoutingModule, FormsModule, ReactiveFormsModule, TranslateModule.forRoot({
+    defaultLanguage: "en",
+    loader: {
+      provide: TranslateLoader,
+      useFactory: httpTranslateLoader,
+      deps: [HttpClient]
+    }
+  })],
 })
 export class AdvertisementModule {}
